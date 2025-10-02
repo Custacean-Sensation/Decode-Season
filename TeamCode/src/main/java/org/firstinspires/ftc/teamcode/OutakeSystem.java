a214ceeda220bc2b1b5e4df7653dd934f2e22145
JakerGMan <jakegjohnson@icloud.com>
2025-10-02 16:51:20 -0500

    Add OutakeSystem class for outake motor control
    Introduces the OutakeSystem class to manage outake motors and servo, including methods to start, stop, and set power for the motors and servo. This provides a way to control the outake mechanism in the robot code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OutakeSystem.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class OutakeSystem {
+    private DcMotor outakeMotorL;
+    private DcMotor outakeMotorR;
+    private boolean outakeOn = false;
+    private boolean servoOn = false;
+    private Servo outakeServo;
+    
+
+    // Constructor
+    public OutakeSystem(HardwareMap hardwareMap, String outakeMotorName, String beltMotorName) {
+        this.outakeMotorL = hardwareMap.get(DcMotor.class, outakeMotorName);
+        this.outakeMotorR = hardwareMap.get(DcMotor.class, beltMotorName);
+
+        // Set motor directions (assuming forward for both)
+        this.outakeMotorL.setDirection(DcMotor.Direction.FORWARD);
+        this.outakeMotorR.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set motor modes
+        this.outakeMotorL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.outakeMotorR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void startOutake() {
+        outakeMotorL.setPower(0.5);
+        outakeMotorR.setPower(-0.5);
+        outakeOn = true;
+    }
+
+    public void stopOutake() {
+        outakeMotorL.setPower(0);
+        outakeMotorR.setPower(0);
+        outakeOn = false;
+    }
+
+    public void startServo() {
+        outakeServo.setPosition(1.0);
+        servoOn = true;
+    }
+
+    public void ReverseServo() {
+        outakeServo.setPosition(0.0);
+        servoOn = false;
+    }
+
+    public boolean isOutakeOn() {
+        return outakeOn;
+    }
+
+    public boolean isServoOn() {
+        return servoOn;
+    }
+
+    public void setOutakePower(double power) {
+        outakeMotorL.setPower(power);
+        outakeOn = power != 0;
+    }
+
+    public void setOutakeRPower(double power) {
+        outakeMotorR.setPower(power);
+        outakeOn = power != 0;
+    }
+}
