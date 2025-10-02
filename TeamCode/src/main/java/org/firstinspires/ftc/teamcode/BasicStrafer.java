c151be31a6e964b625f373917c5ac11e08a96fce
NotMaxwell <william.max.mccormick@gmail.com>
2025-10-02 11:41:23 -0500

    updated basic straffer to use example direvtrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicStrafer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicStrafer.java
@@ -2,42 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
 @TeleOp(name = "BasicStrafer")
 public class BasicStrafer extends LinearOpMode
 {
-    //Declare all of my DRIVE motors
-    public DcMotor foreLeftDrive;
-    public DcMotor foreRightDrive;
-    public DcMotor backLeftDrive;
-    public DcMotor backRightDrive;
-
-    //Declare Power value
-    double foreLeftPower;
-    double foreRightPower;
-    double backLeftPower;
-    double backRightPower;
+    private final ExampleDrivetrain dt = new ExampleDrivetrain(hardwareMap, "LeftFront", "RightFront", "LeftBack", "RightBack");
     public void runOpMode()
     {
-        //craete vars for how I want to move the robot
+        //create vars for how I want to move the robot
         float axial;
         float yaw;
         float lateral;
         double maxPower;
+        double foreLeftPower;
+        double foreRightPower;
+        double backLeftPower;
+        double backRightPower;
 
-        //give motor assignments
-        foreLeftDrive = hardwareMap.get(DcMotor.class, "LeftFront");
-        foreRightDrive = hardwareMap.get(DcMotor.class, "RightFront");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "LeftBack");
-        backRightDrive = hardwareMap.get(DcMotor.class, "RightBack");
+        dt.stop();
 
-        //set motor directions
-        foreLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        foreRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
-        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        
         waitForStart();
         
         while(opModeIsActive())
@@ -52,6 +34,8 @@ public class BasicStrafer extends LinearOpMode
             backLeftPower = (axial + lateral) - yaw;
             backRightPower = (axial - lateral) + yaw;
 
+            dt.setPowers(foreLeftPower, foreRightPower, backLeftPower, backRightPower);
+
             maxPower = Math.max(Math.abs(foreLeftPower), Math.max(Math.abs(foreRightPower), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
             if (maxPower > 1) {
                 foreLeftPower /= maxPower;
@@ -60,10 +44,7 @@ public class BasicStrafer extends LinearOpMode
                 backRightPower /= maxPower;
             }
 
-            foreLeftDrive.setPower(foreLeftPower);
-            foreRightDrive.setPower(foreRightPower);
-            backLeftDrive.setPower(backLeftPower);
-            backRightDrive.setPower(backRightPower);
+            dt.setPowers(foreLeftPower, foreRightPower, backLeftPower, backRightPower);
         }
     }
 }
