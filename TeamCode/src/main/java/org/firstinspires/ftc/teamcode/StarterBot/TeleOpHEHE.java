a1fb2e4816fd49e97f46961b51b7c7af960f0ae6
NotMaxwell <william.max.mccormick@gmail.com>
2025-11-01 11:24:13 -0500

    Working mecanum in TeleOpHEHE.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBot/TeleOpHEHE.java
@@ -0,0 +1,231 @@
+package org.firstinspires.ftc.teamcode.StarterBot;
+
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.ExampleDrivetrain;
+
+@TeleOp(name = "StarterBotTeleop")
+public class TeleOpHEHE extends OpMode {
+    final double FEED_TIME_SECONDS = 0.20; //The feeder servos run this long when a shot is requested.
+    final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
+    final double FULL_SPEED = 1.0;
+
+    /*
+     * When we control our launcher motor, we are using encoders. These allow the control system
+     * to read the current speed of the motor and apply more or less power to keep it at a constant
+     * velocity. Here we are setting the target, and minimum velocity that the launcher should run
+     * at. The minimum velocity is a threshold for determining when to fire.
+     */
+    final double LAUNCHER_TARGET_VELOCITY = 1125;
+    final double LAUNCHER_MIN_VELOCITY = 1075;
+
+    // Declare OpMode members.
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotorEx launcher = null;
+    private CRServo leftFeeder = null;
+    private CRServo rightFeeder = null;
+    private ExampleDrivetrain dt = null;
+
+    ElapsedTime feederTimer = new ElapsedTime();
+
+    /*
+     * TECH TIP: State Machines
+     * We use a "state machine" to control our launcher motor and feeder servos in this program.
+     * The first step of a state machine is creating an enum that captures the different "states"
+     * that our code can be in.
+     * The core advantage of a state machine is that it allows us to continue to loop through all
+     * of our code while only running specific code when it's necessary. We can continuously check
+     * what "State" our machine is in, run the associated code, and when we are done with that step
+     * move on to the next state.
+     * This enum is called the "LaunchState". It reflects the current condition of the shooter
+     * motor and we move through the enum when the user asks our code to fire a shot.
+     * It starts at idle, when the user requests a launch, we enter SPIN_UP where we get the
+     * motor up to speed, once it meets a minimum speed then it starts and then ends the launch process.
+     * We can use higher level code to cycle through these states. But this allows us to write
+     * functions and autonomous routines in a way that avoids loops within loops, and "waits".
+     */
+    private enum LaunchState {
+        IDLE,
+        SPIN_UP,
+        LAUNCH,
+        LAUNCHING,
+    }
+
+    private LaunchState launchState;
+
+    // Setup a variable for each drive wheel to save power level for telemetry
+    double leftPower;
+    double rightPower;
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        launchState = LaunchState.IDLE;
+
+        /*
+         * Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step.
+         */
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        leftFeeder = hardwareMap.get(CRServo.class, "leftFeeder");
+        rightFeeder = hardwareMap.get(CRServo.class, "rightFeeder");
+
+        dt = new ExampleDrivetrain(hardwareMap, "frontLeft", "frontRight", "backLeft", "backRight");
+        /*
+         * Here we set our launcher to the RUN_USING_ENCODER runmode.
+         * If you notice that you have no control over the velocity of the motor, it just jumps
+         * right to a number much higher than your set point, make sure that your encoders are plugged
+         * into the port right beside the motor itself. And that the motors polarity is consistent
+         * through any wiring.
+         */
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        /*
+         * Setting zeroPowerBehavior to BRAKE enables a "brake mode". This causes the motor to
+         * slow down much faster when it is coasting. This creates a much more controllable
+         * drivetrain. As the robot stops much quicker.
+         */
+        launcher.setZeroPowerBehavior(BRAKE);
+
+        /*
+         * set Feeders to an initial value to initialize the servo controller
+         */
+        leftFeeder.setPower(STOP_SPEED);
+        rightFeeder.setPower(STOP_SPEED);
+
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
+
+        /*
+         * Much like our drivetrain motors, we set the left feeder servo to reverse so that they
+         * both work to feed the ball into the robot.
+         */
+        leftFeeder.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        /*
+         * Tell the driver that initialization is complete.
+         */
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits START
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        /*
+         * Here we call a function called arcadeDrive. The arcadeDrive function takes the input from
+         * the joysticks, and applies power to the left and right drive motor to move the robot
+         * as requested by the driver. "arcade" refers to the control style we're using here.
+         * Much like a classic arcade game, when you move the left joystick forward both motors
+         * work to drive the robot forward, and when you move the right joystick left and right
+         * both motors work to rotate the robot. Combinations of these inputs can be used to create
+         * more complex maneuvers.
+         */
+        mecanumDrive(gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
+
+        /*
+         * Here we give the user control of the speed of the launcher motor without automatically
+         * queuing a shot.
+         */
+        if (gamepad1.y) {
+            launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+        } else if (gamepad1.b) { // stop flywheel
+            launcher.setVelocity(STOP_SPEED);
+        }
+
+        if(gamepad1.leftBumperWasPressed()) {
+            feederTimer.reset();
+            rightFeeder.setPower(FULL_SPEED);
+            leftFeeder.setPower(FULL_SPEED);
+            while (feederTimer.seconds() < FEED_TIME_SECONDS) {}
+            rightFeeder.setPower(STOP_SPEED);
+            leftFeeder.setPower(STOP_SPEED);
+        }
+
+        /*
+         * Now we call our "Launch" function.
+         */
+        launch(gamepad1.rightBumperWasPressed());
+
+        /*
+         * Show the state and motor powers
+         */
+        telemetry.addData("State", launchState);
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("motorSpeed", launcher.getVelocity());
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+    void mecanumDrive(double forward, double rotate, double yaw) {
+        double foreLeftPower = (forward + rotate) + yaw;
+        double foreRightPower = (forward - rotate) - yaw;
+        double backLeftPower = (forward - rotate) + yaw;
+        double backRightPower = (forward + rotate) - yaw;
+
+        dt.setPowers(foreLeftPower, foreRightPower, backLeftPower, backRightPower);
+    }
+
+    void launch(boolean shotRequested) {
+        switch (launchState) {
+            case IDLE:
+                if (shotRequested) {
+                    launchState = LaunchState.SPIN_UP;
+                }
+                break;
+            case SPIN_UP:
+                launcher.setVelocity(LAUNCHER_TARGET_VELOCITY);
+                if (launcher.getVelocity() > LAUNCHER_MIN_VELOCITY) {
+                    launchState = LaunchState.LAUNCH;
+                }
+                break;
+            case LAUNCH:
+                leftFeeder.setPower(FULL_SPEED);
+                rightFeeder.setPower(FULL_SPEED);
+                feederTimer.reset();
+                launchState = LaunchState.LAUNCHING;
+                break;
+            case LAUNCHING:
+                if (feederTimer.seconds() > FEED_TIME_SECONDS) {
+                    launchState = LaunchState.IDLE;
+                    leftFeeder.setPower(STOP_SPEED);
+                    rightFeeder.setPower(STOP_SPEED);
+                }
+                break;
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBot/testAuto.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.StarterBot;
-
-public class testAuto {
-
-}
