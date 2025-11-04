d46915d2bba8a6894cc4c0b37022eafd6108821f
NotMaxwell <william.max.mccormick@gmail.com>
2025-11-04 17:02:31 -0600

    Created a basic limelight file to read a april tag and output information based on it.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLimelightCalls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicLimelightCalls.java
@@ -7,31 +7,33 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 public class BasicLimelightCalls extends LinearOpMode {
+
     private Limelight3A limelight;
+
     @Override
     public void runOpMode() throws InterruptedException
     {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
         telemetry.setMsTransmissionInterval(11);
+
         limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
         limelight.start();
 
-        while(opModeIsActive())
-        {
+        while (opModeIsActive()) {
             LLResult result = limelight.getLatestResult();
-            if (result != null)
-            {
-                if (result.isValid())
-                {
+            if (result != null) {
+                if (result.isValid()) {
                     Pose3D botpose = result.getBotpose();
                     telemetry.addData("tx", result.getTx());
                     telemetry.addData("ty", result.getTy());
                     telemetry.addData("Botpose", botpose.toString());
                 }
             }
-
-            //rest of code for driving
         }
     }
 }
