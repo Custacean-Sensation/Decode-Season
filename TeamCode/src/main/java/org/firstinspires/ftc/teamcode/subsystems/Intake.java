d6c5ec11e204560eb47ca6551bf1f2bfbfb19dcf
KaosMaker9 <nsifuentes09@gmail.com>
2025-11-20 14:03:00 -0600

    Fixed Intake, had syntax errors and used an unusable method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -8,7 +8,7 @@ public class Intake {
 
     private DcMotor intakeMotor;
     private CRServo leftFeed;
-    private CRServo rightFreed;
+    private CRServo rightFeed;
 
     private double intakePower = 1.0; 
     private double feedPower = 1.0;   
@@ -17,35 +17,33 @@ public class Intake {
     public Intake(HardwareMap hardwareMap, String intakeMotorName, String leftFeedName, String rightFeedName) {
         intakeMotor = hardwareMap.get(DcMotor.class, intakeMotorName); 
         leftFeed = hardwareMap.get(CRServo.class, leftFeedName);
-        righFeed = hardwareMap.get(CRServo.class, rightFeedName) 
+        rightFeed = hardwareMap.get(CRServo.class, rightFeedName);
 
         intakeMotor.setDirection(DcMotor.Direction.FORWARD);
         leftFeed.setDirection(CRServo.Direction.FORWARD);
         rightFeed.setDirection(CRServo.Direction.FORWARD);
 
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFeed.setZeroPowerBehavior(CRServo.ZeroPowerBehavior.BRAKE);
-        rightFeed.setZeroPowerBehavior(CRServo.ZeroPowerBehavior.BRAKE);
     }
 
     // Start both intake and belt
     public void start() {
         intakeMotor.setPower(intakePower);
         leftFeed.setPower(feedPower);
-        righFeed.setPower(feedPower);
+        rightFeed.setPower(feedPower);
     }
 
     // Spit out artifact(if possible)
     public void reverse() {
         intakeMotor.setPower(-intakePower);
-        leftFeed.setPower(-feedPower)
-        righFeed.setPower(-feedPower);
+        leftFeed.setPower(-feedPower);
+        rightFeed.setPower(-feedPower);
     }
 
     // Stop both intake and belt
     public void stop() {
         intakeMotor.setPower(0);
         leftFeed.setPower(0);
-        righFeed.setPower(0);
+        rightFeed.setPower(0);
     }
 }
