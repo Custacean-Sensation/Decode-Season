881801212da72e1d9ceb9004a582210b4444a626
Nick <nsifuentes09@gmail.com>
2025-11-20 09:45:32 -0600

    Servos + Spit Method
    Added CRServos to replace the belt(Hardware's fault), and added a spit() method to spit out the artifacts if we pick up a 4th.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,37 +1,51 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
 
     private DcMotor intakeMotor;
-    private DcMotor beltMotor;
+    private CRServo leftFeed;
+    private CRServo rightFreed;
 
     private double intakePower = 1.0; 
-    private double beltPower = 1.0;   
+    private double feedPower = 1.0;   
 
     // Constructor
-    public Intake(HardwareMap hardwareMap, String intakeMotorName, String beltMotorName) {
+    public Intake(HardwareMap hardwareMap, String intakeMotorName, String leftFeedName, String rightFeedName) {
         intakeMotor = hardwareMap.get(DcMotor.class, intakeMotorName); 
-        beltMotor = hardwareMap.get(DcMotor.class, beltMotorName); 
+        leftFeed = hardwareMap.get(CRServo.class, leftFeedName);
+        righFeed = hardwareMap.get(CRServo.class, rightFeedName) 
 
         intakeMotor.setDirection(DcMotor.Direction.FORWARD);
-        beltMotor.setDirection(DcMotor.Direction.FORWARD);
+        leftFeed.setDirection(CRServo.Direction.FORWARD);
+        rightFeed.setDirection(CRServo.Direction.FORWARD);
 
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        beltMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFeed.setZeroPowerBehavior(CRServo.ZeroPowerBehavior.BRAKE);
+        rightFeed.setZeroPowerBehavior(CRServo.ZeroPowerBehavior.BRAKE);
     }
 
     // Start both intake and belt
     public void start() {
         intakeMotor.setPower(intakePower);
-        beltMotor.setPower(beltPower);
+        leftFeed.setPower(feedPower);
+        righFeed.setPower(feedPower);
+    }
+
+    // Spit out artifact(if possible)
+    public void reverse() {
+        intakeMotor.setPower(-intakePower);
+        leftFeed.setPower(-feedPower)
+        righFeed.setPower(-feedPower);
     }
 
     // Stop both intake and belt
     public void stop() {
         intakeMotor.setPower(0);
-        beltMotor.setPower(0);
+        leftFeed.setPower(0);
+        righFeed.setPower(0);
     }
 }
