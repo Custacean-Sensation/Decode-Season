881f8f824844819cc016b8fb87dab404da520f05
JakerGMan <jakegjohnson@icloud.com>
2025-12-08 16:08:27 -0600

    Add launch method and feeder control to OutakeSystem
    Introduced a launch() method to control feeder servos for Launching, using ElapsedTime for timing. Added new hardware references and constants to support feeder and launcher operations.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OutakeSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/OutakeSystem.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class OutakeSystem {
     private DcMotor outakeMotorL;
@@ -10,7 +11,21 @@ public class OutakeSystem {
     private boolean outakeOn = false;
     private boolean servoOn = false;
     private Servo outakeServo;
+
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotorEx launcher = null;
+    private CRServo leftFeeder = null;
+    private CRServo rightFeeder = null;
+    private ExampleDrivetrain dt = null;
     
+    
+    ElapsedTime feederTimer = new ElapsedTime();
+
+    public class TeleOpHEHE extends OpMode {
+    final double FEED_TIME_SECONDS = 0.20; //The feeder servos run this long when a shot is requested.
+    final double STOP_SPEED = 0.0; //We send this power to the servos when we want them to stop.
+    final double FULL_SPEED = 1.0;
 
     // Constructor
     public OutakeSystem(HardwareMap hardwareMap, String outakeMotorName, String beltMotorName) {
@@ -64,5 +79,15 @@ public class OutakeSystem {
     public void setOutakeRPower(double power) {
         outakeMotorR.setPower(power);
         outakeOn = power != 0;
+    }
+
+    public void launch() {
+        feederTimer.reset();
+        rightFeeder.setPower(FULL_SPEED);
+        leftFeeder.setPower(FULL_SPEED);
+        while (feederTimer.seconds() < FEED_TIME_SECONDS) {}
+        rightFeeder.setPower(STOP_SPEED);
+        leftFeeder.setPower(STOP_SPEED);
+        
     }
 }
